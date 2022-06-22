0357f6c9a244c8db74911d05859424c63462a047
abitale <sendmail.ale@gmail.com>
2022-06-22 15:54:48 +0700

    test

--- model/response/response.go
+++ model/response/response.go
@@ -3,15 +3,15 @@ package response
 import "github.com/api-abc/internal-api-module/model/domain"
 
 type BodyResponse struct {
-	Status  int         `json:"status"`
-	Message string      `json:"message"`
-	Data    domain.Data `json:"data,omitempty"`
+	Status  int          `json:"status"`
+	Message string       `json:"message"`
+	Data    *domain.Data `json:"data,omitempty"`
 }
 
 type BodyResponseGet struct {
-	Status  int           `json:"status"`
-	Message string        `json:"message"`
-	Data    []domain.Data `json:"data,omitempty"`
+	Status  int            `json:"status"`
+	Message string         `json:"message"`
+	Data    []*domain.Data `json:"data,omitempty"`
 }
 
 const (
